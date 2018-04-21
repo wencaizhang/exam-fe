8778c13339f3dda11425508ceef464c56062ba0e
wencaizhang <1052642137@qq.com>
2018-04-21 12:51:45 +0800

    增加返回Header“

---
+++ src/components/BackHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <flexbox>
+      <flexbox-item>
+        <x-button class="return" mini type="primary" link="BACK">返回</x-button>
+      </flexbox-item>
+    </flexbox>
+  </div>
+</template>
+
+<script>
+import { XButton, Flexbox, FlexboxItem } from "vux";
+export default {
+  components: {
+    XButton,
+    Flexbox,
+    FlexboxItem
+  }
+};
+</script>
+<style>
+.header {
+  padding: 10px;
+}
+</style>
