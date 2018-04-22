86ed4f6e103b49fd6785cfae8138061c9f97c028
wencaizhang <1052642137@qq.com>
2018-04-22 14:36:02 +0800

    update register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,13 +6,10 @@
         </div>
         <div class="clearfix infoBox">
             <label for="" class="name">所属单位:</label>
-            <div class="infoInput companyInfo">
-                <div @click="showPic">
-                    <group-title class="selectInput">{{companyName}}</group-title>
-                </div>
-                <picker :data="danwei" :columns="3" :show="showPicker" v-model="companyName"></picker>
+            <div  @click="showPic" class="infoInput companyInfo">
+                <group-title class="selectInput">{{companyName}}</group-title>
             </div>
-        </div>
+        </div> 
         <div class="clearfix infoBox">
             <label for="" class="name user_name">用户名:</label>
             <div class="infoInput"><input type="text"></div>
@@ -33,30 +30,48 @@
             <label for="" class="name">确认密码:</label>
             <div class="infoInput"><input type="text"></div>
         </div>
+
+        <div v-transfer-dom>
+          <popup v-model="showPopup" @on-hide="setCompanyName">
+            <div class="popup0">
+              <picker :data='danwei' :columns=3 v-model='danweiValue'></picker>
+            </div>
+          </popup>
+        </div>
     </form>
 </template>
 
 <script>
 import Vue from "vue";
 import axios from "axios";
-import { Picker, GroupTitle } from "vux";
+import {
+  PopupPicker,
+  Picker,
+  Popup,
+  Group,
+  GroupTitle,
+  TransferDom
+} from "vux";
 
 export default {
   name: "Register",
+  directives: {
+    TransferDom
+  },
   data() {
     return {
       tips: "",
-      showPicker: false,
+      title: "所属单位：",
       danwei: [
         {
           name: "中国",
           value: "china",
-          parent: 0
+          parent: "0" // 为一级时可以不写 parent，但是此时允许为数字 0、空字符串或者字符串 '0'
         },
         {
           name: "美国",
           value: "USA",
-          parent: 0
+          parent: "0"
         },
         {
           name: "广东",
@@ -90,12 +105,12 @@ export default {
         },
         {
           name: "广西001",
-          value: "gz",
+          value: "gx001",
           parent: "china002"
         },
         {
           name: "广西002",
-          value: "sz",
+          value: "gx002",
           parent: "china002"
         },
         {
@@ -119,16 +134,35 @@ export default {
           parent: "usa002"
         }
       ],
-      companyName: null,
+      danweiValue: [],
+      showPopup: false,
+      showPicker: true,
+      companyName: ''
     };
   },
   components: {
+    PopupPicker,
     Picker,
+    Group,
+    Popup,
     GroupTitle
   },
   methods: {
+    setCompanyName () {
+      const arr = [];
+      const { danwei, danweiValue } = this;
+      let name = '';
+      danweiValue.forEach(function(item, index) {
+        danwei.forEach(function(item2) {
+          if (item2.value === item && index + 1 === danweiValue.length) {
+            name += item2.name
+          }
+        })
+      })
+      this.companyName = name.trim();
+    },
     showPic() {
-        this.showPicker = !this.showPicker;
+      this.showPopup = true;
     }
   }
 };
