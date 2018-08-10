097657d467f8bb168038a1f32e8d2fdea84a08c9
wencaizhang <1052642137@qq.com>
2018-08-10 23:44:53 +0800

    api.js 封装

--- src/util/api.js
+++ src/util/api.js
@@ -1,26 +1,53 @@
 
 import axios from "axios";
 
+const postData = ( data=null, url='', callback=null ) => {
+    const options = {
+        url,
+        method: "POST",
+        data,
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+    };
+
+    axios(options)
+    .then( resp => {
+        if (resp.data.code == 0) {
+            callback && callback(resp.data)
+        } else {
+            alert(resp.data.msg);
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+const getData = ( data=null, url='', callback=null ) => {
+    const options = {
+        url,
+        method: "GET",
+        data,
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+    };
+
+    axios(options)
+    .then( resp => {
+        if (resp.data.code == 0) {
+            callback && callback(resp.data)
+        } else {
+            alert(resp.data.msg);
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
 const api = {
     logout: (data=null, callback) => {
         let url = '/logout';
 
-        const options = {
-            url,
-            method: "GET",
-            data,
-            headers: { "content-type": "application/x-www-form-urlencoded" },
-        };
-
-        axios(options)
-        .then( resp => {
-            if (resp.data.code == 0) {
-                callback && callback(resp.data)
-            }
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+        getData(null, url, callback)
     },
 };
 
