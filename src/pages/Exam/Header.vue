5199b7c5ac80517f7fc3b1ac9dc817f6a4c792a7
wencaizhang <1052642137@qq.com>
2018-08-10 21:29:10 +0800

    增加考试离开的风险提示

--- src/pages/Exam/Header.vue
+++ src/pages/Exam/Header.vue
@@ -1,17 +1,48 @@
 <template>
-  <x-header  :left-options="{preventGoBack: true}" @on-click-back="backHandler">{{ time }}<a slot="right" @click="togglePause">{{ buttonText }}</a></x-header>
+
+  <div>
+    <x-header 
+      :left-options="{preventGoBack: true}" 
+      @on-click-back="backHandler"
+    >
+      {{ time }}
+      <a slot="right" @click="togglePause">{{ buttonText }}</a>
+    </x-header>
+    
+    <div v-transfer-dom>
+      <confirm 
+        v-model="showModal"
+        title="提示"
+        confirm-text="确定退出"
+        cancel-text="继续答题"
+        @on-confirm="onConfirm"
+      >
+        <p style="text-align:center;">您即将放弃本次考试</p>
+      </confirm>
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import { XHeader  } from "vux";
+import {
+  Confirm,
+  XHeader,
+  TransferDomDirective as TransferDom
+} from "vux";
 export default {
+  directives: {
+    TransferDom
+  },
   data() {
     return {
-      seconds: 0
+      seconds: 0,
+      showModal: false
     };
   },
   components: {
     XHeader,
+    Confirm
   },
   computed: {
     time () {
@@ -40,7 +71,10 @@ export default {
 
     },
     backHandler () {
-      this.$router.push( { name: 'waitforexam' })
+      this.showModal = true;
+    },
+    onConfirm () {
+      this.$router.push( { name: 'waitforexam' });
     }
   },
   mounted () {
