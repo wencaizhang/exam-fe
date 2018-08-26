77e54f01ba3eccfc7f04aed1a101a5a46e32a749
wencaizhang <1052642137@qq.com>
2018-08-26 14:15:54 +0800

    重置密码页面完善

--- src/pages/User/ResetPwd.vue
+++ src/pages/User/ResetPwd.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <x-header :left-options="{backText: ''}">重置密码</x-header>
+    <x-header v-if="!showMsg"  :left-options="{backText: ''}">重置密码</x-header>
     <form v-if="!showMsg" :class="$style.container">
       <div :class="$style.input_wrap">
-        <input :class="$style.input" type="password" placeholder="原密码" v-model="password" autofocus>
+        <span :class="[$style.icon_wrap, $style.eye]" v-show="password && showpassword" @click="toggleShowPwd('password')"></span>
+        <span :class="[$style.icon_wrap, $style.eye2]" v-show="password && !showpassword" @click="toggleShowPwd('password')"></span>
+        <input :class="$style.input" :type="showpassword ? 'text' : 'password'" placeholder="原密码" v-model="password" autofocus ref="password">
       </div>
       <div :class="$style.input_wrap">
-        <input :class="$style.input" type="password" placeholder="新密码" v-model="newPassword1">
+        <input :class="$style.input" :type="shownewpassword1 ? 'text' : 'password'" placeholder="新密码" v-model="newPassword1" ref="newpassword1">
+        <span :class="[$style.icon_wrap, $style.eye]" v-show="newPassword1 && shownewpassword1" @click="toggleShowPwd('newpassword1')"></span>
+        <span :class="[$style.icon_wrap, $style.eye2]" v-show="newPassword1 && !shownewpassword1" @click="toggleShowPwd('newpassword1')"></span>
       </div>
       <div :class="$style.input_wrap">
-        <input :class="$style.input" type="password" placeholder="确认新密码" v-model="newPassword2">
+        <input :class="$style.input" :type="shownewpassword2 ? 'text' : 'password'" placeholder="确认新密码" v-model="newPassword2" ref="newpassword2">
+        <span :class="[$style.icon_wrap, $style.eye]" v-show="newPassword2 && shownewpassword2" @click="toggleShowPwd('newpassword2')"></span>
+        <span :class="[$style.icon_wrap, $style.eye2]" v-show="newPassword2 && !shownewpassword2" @click="toggleShowPwd('newpassword2')"></span>
       </div>
       <XButton
         text="重置密码" 
@@ -31,12 +37,16 @@ export default {
       password: '',
       newPassword1: '',
       newPassword2: '',
+
+      showpassword: false,
+      shownewpassword1: false,
+      shownewpassword2: false,
       
       showMsg: false,
       buttons: [{
         type: 'primary',
         text: '重新登录',
-        onClick: this.logout.bind(this)
+        onClick: this.toLogin.bind(this)
       }],
     };
   },
@@ -46,13 +56,13 @@ export default {
     Msg
   },
   methods: {
-    logout() {
-      this.$store.dispatch('logout').then(resp => {
-        if (resp.data.code == 0) {
-          this.$store.dispatch('resetAllState')
-          this.$router.push({ path: "/login" });
-        }
-      });
+    toLogin() {
+      this.$router.push({ path: "/login" });
+    },
+    toggleShowPwd (ref) {
+      console.log('ref', ref)
+      this['show' + ref] = !this['show' + ref];
+      this.$refs[ref].focus();
     },
     submit() {
       const { password, newPassword1, newPassword2 } = this;
@@ -76,25 +86,30 @@ export default {
           this.$vux.toast.show({
             text: resp.data.msg
           });
+          this.$store.dispatch('logout').then(resp => {
+            if (resp.data.code == 0) {
+              this.$store.dispatch('resetAllState')
+            }
+          });
           this.showMsg = true;
         }
       })
     },
-  }
+  },
 };
 </script>
 
 <style module>
 .container {
   text-align: center;
-  padding: 40px;
   padding-top: 20px;
   letter-spacing: 1px;
-
+  width: 80%;
   /* 垂直居中 */
   position: absolute;
   top: 50%;
-  transform: translateY(-50%);
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 .input_wrap {
   position: relative;
@@ -113,5 +128,25 @@ export default {
 }
 .input:focus {
   box-shadow:none;
+}
+
+.icon_wrap {
+  position: absolute;
+  right: 4px;
+  top: 2px;
+  z-index: 10000;
+  display: inline-block;
+  border-radius: 50%;
+  background: #f2f2f2;
+  width: 20px;
+  height: 20px;
+}
+.eye {
+  background: url(../../assets/images/eye.png);
+  background-size: cover;
+}
+.eye2 {
+  background: url(../../assets/images/eye2.png);
+  background-size: cover;
 }
 </style>
