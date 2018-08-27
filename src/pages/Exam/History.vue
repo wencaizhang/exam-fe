93d8a009ef7a4ba9337cd47f49f63ee2c720935b
wencaizhang <1052642137@qq.com>
2018-08-27 23:19:42 +0800

    增加模拟考试成绩列表

--- src/pages/Exam/History.vue
+++ src/pages/Exam/History.vue
@@ -3,42 +3,59 @@
         <x-header>
             考试成绩
         </x-header>
-        <scroller style="margin-top: 44px"
-            ref="my_scroller"
-            :on-refresh="refresh"
-            :on-infinite="infinite"
+        <tab>
+          <tab-item selected @on-item-click="clickHandler">正式考试</tab-item>
+          <tab-item badge-background="#38C972" badge-color="#fff" @on-item-click="clickHandler">模拟考试</tab-item>
+        </tab>
+        <scroller style="margin-top: 90px"
+          ref="my_scroller"
+          :on-refresh="refresh"
+          :on-infinite="infinite"
         >
-            <group class="row" v-for="(item, index) in list" v-bind:key="index" >
-                <cell :title="item.paperName"></cell>
-                <cell-form-preview :list="item.list"></cell-form-preview>
-            </group>
+          <group class="row" v-for="(item, index) in currList" v-bind:key="index" >
+            <cell :title="item.paperName"></cell>
+            <cell-form-preview :list="item.list"></cell-form-preview>
+          </group>
         </scroller>
     </div>
 </template>
 <script>
 import axios from "axios";
-import { XHeader, CellFormPreview, Group, Cell, dateFormat } from "vux";
+import { XHeader, CellFormPreview, Group, Cell, dateFormat,Tab, TabItem } from "vux";
 export default {
   data() {
     return {
-      list: [],
-      page: 0,
-      pageSize: 10
+      list0: [],
+      list1: [],
+      page0: 0,
+      page1: 0,
+      pageSize: 10,
+      index: 0,
     };
   },
   components: {
     XHeader,
     CellFormPreview,
     Group,
-    Cell
+    Cell,
+    Tab,
+    TabItem
   },
   created() {
-    this.page = 0;
+    this.page0 = 0;
+    this.page1 = 0;
+  },
+  computed: {
+    currList () {
+      return this.index === 0 ? this.list0 : this.list1;
+    }
   },
   methods: {
     fetch() {
-      let url = "/sage/exam/score/selectScoreList";
-      const data = { page: this.page, pageSize: this.pageSize };
+      let url0 = '/sage/exam/score/selectScoreList';
+      let url1 = '/sage/exam/simulationscore/selectScoreList';
+      let url = this.index === 0 ? url0 : url1;
+      const data = { page: this['page' + this.index], pageSize: this.pageSize };
       return axios.post(url, data);
     },
     add(m){return m<10?'0'+m:m },
@@ -82,25 +99,31 @@ export default {
       return list;
     },
     refresh() {
-      this.page = 1;
+      this['page' + this.index] = 1;
       this.fetch().then(resp => {
         if (resp.data.code == 0) {
-          this.list = this.transformData(resp.data.scoreList.list);
+          this['list' + this.index] = this.transformData(resp.data.scoreList.list);
           let length = resp.data.scoreList.list.length;
           this.$refs.my_scroller.finishPullToRefresh();
         }
       });
     },
     infinite() {
-      this.page++;
+      this['page' + this.index]++;
       this.fetch().then(resp => {
         if (resp.data.code == 0) {
 
-          this.list = this.list.concat(this.transformData(resp.data.scoreList.list));
+          this['list' + this.index] = this['list' + this.index].concat(this.transformData(resp.data.scoreList.list));
           let length = resp.data.scoreList.list.length;
           this.$refs.my_scroller.finishInfinite(!length);
         }
       });
+    },
+    clickHandler(index) {
+      this.index = index;
+      if (!this['list' + this.index].length) {
+        this.refresh();
+      }
     }
   }
 };
