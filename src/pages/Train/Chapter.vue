ecaf59caee532ecaeca32853960415780c6e7653
wencaizhang <1052642137@qq.com>
2018-09-05 23:32:25 +0800

    Create Chapter

---
+++ src/pages/Train/Chapter.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div :class="$style.container">
+      <group>
+        <cell-box >
+          章节
+        </cell-box>
+      </group>
+    </div>
+    
+</template>
+
+<script>
+import axios from "axios";
+import { XButton, Group, CellBox, Cell, Badge } from "vux";
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    Group,
+    CellBox,
+    Cell,
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+};
+</script>
+
+<style >
+</style>
